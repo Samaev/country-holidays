726347f2e519274ae3cb5f0559dc07de3b1304de
AndreySamaev <andreysamaev@gmail.com>
2024-10-14 18:28:38 +0300

    country template, search

---
+++ pages/[country].vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+new country-holidays
+</template>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,14 @@ import { ref } from 'vue';
 import { useCountries } from '~/composables/useCountries.ts';
 
 const countries = ref([]);
+const searchQuery = ref('');
 const error = ref(null);
+
+const filteredCountries = computed(()=>{
+  return countries.value.filter(country=>{
+    return country.name.toLowerCase().includes(searchQuery.value.toLowerCase().trim());
+  });
+});
 
 try {
   countries.value = await useCountries();
@@ -17,15 +24,21 @@ try {
     <div class="container-fluid row">
       <section class="cards col-3">
         <div class="cards__search">
-          <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
+          <form class="d-flex" @submit.prevent>
+            <input class="form-control me-2" type="search" v-model="searchQuery" placeholder="Search" aria-label="Search">
           </form>
         </div>
-        <div class="cards__list mt-2">
-          <div v-for="country in countries" :key="country.countryCode" class="card p-2 my-1">
+        <div class="cards__list mt-2" v-if="filteredCountries.length">
+          <div v-for="country in filteredCountries" :key="country.countryCode" class="card p-2 my-1">
             {{ country.name }}
           </div>
+        </div>
+        <div v-else-if="error">{{ error.message }}</div>
+        <div v-else>
+          <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+          <div>Loading countries...</div>
         </div>
       </section>
       <section class="widget col">

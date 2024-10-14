2b1fd942503b2c7651522436ce1dfa98c6f53868
AndreySamaev <andreysamaev@gmail.com>
2024-10-14 22:00:47 +0300

    update sections with sticky

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
  <template>
    <nav class="navbar navbar-light bg-light container">
-     <div class="container-fluid">
+     <div class="container-fluid overflow-hidden">
        <NuxtLink class="navbar-brand" to="/">Homepage</NuxtLink>
        <NuxtLink class="navbar-brand" to="/">International holidays from all over the World</NuxtLink>
      </div>
--- pages/index.vue
+++ pages/index.vue
@@ -39,14 +39,23 @@ try {
 <template>
   <div class="container">
     <div class="container-fluid row">
-      <section class="cards col-3">
+      <section class="cards col-5 col-md-3">
         <div class="cards__search">
-          <input class="form-control" type="search" v-model.trim="searchQuery" placeholder="Search"
-                 aria-label="Search">
+          <input
+              class="form-control"
+              type="search"
+              v-model.trim="searchQuery"
+              placeholder="Search"
+              aria-label="Search"
+          />
         </div>
         <div class="cards__list mt-2" v-if="filteredCountries.length">
-          <NuxtLink v-for="country in filteredCountries" :key="country.countryCode" class="card p-2 my-1"
-                    :to="`/${country.countryCode}`">
+          <NuxtLink
+              v-for="country in filteredCountries"
+              :key="country.countryCode"
+              class="card p-2 my-1"
+              :to="`/${country.countryCode}`"
+          >
             {{ country.name }}
           </NuxtLink>
         </div>
@@ -58,12 +67,28 @@ try {
           <div>Loading countries...</div>
         </div>
       </section>
-      <section class="widget col">
+
+      <section class="widget col-7 col-md-9 sticky-widget">
         <h3 class="lead text-center">Next Holidays</h3>
-        <div class="card mb-3" v-for="(holiday,index) in countryWidget" :key="index">
-         <HolidayCard :holiday=holiday :country="holiday.countryName" />
+        <div class="card mb-3" v-for="(holiday, index) in countryWidget" :key="index">
+          <HolidayCard :holiday="holiday" :country="holiday.countryName" />
         </div>
       </section>
     </div>
   </div>
+
 </template>
+
+<style scoped>
+.sticky-widget {
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  background-color: white;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  padding: 1rem;
+  height: 100vh;
+  overflow-y: auto;
+}
+</style>
